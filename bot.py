cb2bef0f6ce96b830097241ec1c6de846085291f
Viktor Yurchenko <urchnk@gmail.com>
2024-08-21 21:01:13 +0200

    Webhooks

--- bot.py
+++ bot.py
@@ -28,24 +28,24 @@ _ = gettext
 TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 
-# DEBUG = env.bool("DEBUG")
-#
-# if not DEBUG:
-#     HEROKU_APP_NAME = env.str("HEROKU_APP_NAME")
-#     # Base URL for webhook will be used to generate webhook URL for Telegram,
-#     # in this example it is used public address with TLS support
-#     BASE_WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com"
-#
-# # Webserver settings
-# # bind localhost only to prevent any external access
-# WEB_SERVER_HOST = "127.0.0.1"
-# # Port for incoming request from reverse proxy. Should be any available port
-# WEB_SERVER_PORT = 8080
-#
-# # Path to webhook route, on which Telegram will send requests
-# WEBHOOK_PATH = "/webhook"
-# # Secret key to validate requests from Telegram (optional)
-# WEBHOOK_SECRET = env.str("WEBHOOK_SECRET")
+DEBUG = env.bool("DEBUG")
+
+if not DEBUG:
+    HEROKU_APP_NAME = env.str("HEROKU_APP_NAME")
+    # Base URL for webhook will be used to generate webhook URL for Telegram,
+    # in this example it is used public address with TLS support
+    BASE_WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com"
+
+# Webserver settings
+# bind localhost only to prevent any external access
+WEB_SERVER_HOST = "127.0.0.1"
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = 8080
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = "/webhook"
+# Secret key to validate requests from Telegram (optional)
+WEBHOOK_SECRET = env.str("WEBHOOK_SECRET")
 
 
 def setup_django():
@@ -62,7 +62,6 @@ bot = Bot(
     token=TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML),
 )
-bot.delete_webhook()
 dp = Dispatcher(storage=storage)
 
 if settings.DEBUG:
@@ -102,55 +101,55 @@ def include_all_routers(_dp: Dispatcher):
     )
 
 
-# async def on_startup(bot: Bot) -> None:
-#     # In case when you have a self-signed SSL certificate, you need to send the certificate
-#     # itself to Telegram servers for validation purposes
-#     # (see https://core.telegram.org/bots/self-signed)
-#     # But if you have a valid SSL certificate, you SHOULD NOT send it to Telegram servers.
-#     await bot.set_webhook(
-#         f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
-#         secret_token=WEBHOOK_SECRET,
-#     )
-#
-#
-# async def on_shutdown():
-#     logging.info("Shutting down...")
-#     await bot.delete_webhook()
-#     logging.info("Bye!")
-#
-#
-# async def main_webhook():
-#     setup_django()
-#
-#     logging.basicConfig(
-#         level=logging.INFO,
-#         format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
-#     )
-#     logger.info("Starting bot")
-#
-#     include_all_routers(dp)
-#
-#     dp.startup.register(on_startup)
-#     dp.shutdown.register(on_shutdown)
-#
-#     app = web.Application()
-#
-#     # Create an instance of request handler,
-#     # aiogram has few implementations for different cases of usage
-#     # In this example we use SimpleRequestHandler which is designed to handle simple cases
-#     webhook_requests_handler = SimpleRequestHandler(
-#         dispatcher=dp,
-#         bot=bot,
-#         secret_token=WEBHOOK_SECRET,
-#     )
-#     # Register webhook handler on application
-#     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-#
-#     # Mount dispatcher startup and shutdown hooks to aiohttp application
-#     setup_application(app, dp, bot=bot)
-#
-#     # And finally start webserver
-#     web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+async def on_startup(bot: Bot) -> None:
+    # In case when you have a self-signed SSL certificate, you need to send the certificate
+    # itself to Telegram servers for validation purposes
+    # (see https://core.telegram.org/bots/self-signed)
+    # But if you have a valid SSL certificate, you SHOULD NOT send it to Telegram servers.
+    await bot.set_webhook(
+        f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
+        secret_token=WEBHOOK_SECRET,
+    )
+
+
+async def on_shutdown():
+    logging.info("Shutting down...")
+    await bot.delete_webhook()
+    logging.info("Bye!")
+
+
+async def main_webhook():
+    setup_django()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
+    logger.info("Starting bot")
+
+    include_all_routers(dp)
+
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    app = web.Application()
+
+    # Create an instance of request handler,
+    # aiogram has few implementations for different cases of usage
+    # In this example we use SimpleRequestHandler which is designed to handle simple cases
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    # Register webhook handler on application
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Mount dispatcher startup and shutdown hooks to aiohttp application
+    setup_application(app, dp, bot=bot)
+
+    # And finally start webserver
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 
 async def main():
@@ -173,6 +172,6 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        asyncio.run(main_webhook())
     except (KeyboardInterrupt, SystemExit):
         logger.error("Bot stopped!")
